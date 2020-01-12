b5bdd8bbbe8b8f8740ca4a8e0ebce5556fc3f246
ctslone <ctslone27@gmail.com>
2020-01-12 17:37:01 -0500

    initial commit and adding basic server and db connections

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require axios and cheerio. This makes the scraping possible
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
+
+// Database configuration
+var databaseUrl = "steamdb";
+var collections = ["steamData"];
+
+// Hook mongojs configuration to the db variable
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+// Listen on port 3000
+app.listen(3000, function() {
+    console.log("App running on port 3000!");
+  });
