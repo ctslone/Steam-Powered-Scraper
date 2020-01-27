5ee819761b16f4bcd1d97edcb379ad23d5a524e4
ctslone <ctslone27@gmail.com>
2020-01-26 22:23:53 -0500

    removing old files

--- routes/steamRoutes.js
+++ routes/steamRoutes.js
@@ -1,12 +1,1 @@
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// will need a route for new, top, current and upcoming games
-// need a route for saved articles
-    // when user clicks the save button, it uses some verson of this.(id?) and takes the ID saved on the card and button and then adds that particular game to the saved table
-// need a table for each type of game
-// need a table for saved games
-
-module.exports = function(app) {
-
-}
+// not used yet
